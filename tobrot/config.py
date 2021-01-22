5d8bfbd6a62ba9a49a27b46e871d9b424772ee72
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-22 09:42:08 -0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -7,3 +7,6 @@ class Config(Config):
     APP_ID = 1383845 #imp
     API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c" #imp
     AUTH_CHANNEL = [-1001367838165, 1131653685] #imp replace your_id with your id from telegram or delete
+    R_CLONE_CONF_URI = "https://t.me/c/1329348574/566"
+    R_CLONE_DEST = "/"
+    
