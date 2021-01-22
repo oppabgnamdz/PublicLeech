631e780e189ef20418516a6eeb0acef8654a247d
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-22 09:38:20 -0800

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,9 @@
+from tobrot.sample_config import Config
+
+#read readme too before filling these stuffs
+
+class Config(Config):
+    TG_BOT_TOKEN= "1545841239:AAEWFgrwdKsLrxVXBTJlgpqzTIAIBceQvDw" #imp
+    APP_ID = 1383845 #imp
+    API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c" #imp
+    AUTH_CHANNEL = [-1001367838165, 1131653685] #imp replace your_id with your id from telegram or delete
