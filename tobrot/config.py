f0f2ee4af468f5c6d6b9728cce781cd3151c6d80
oppabgnamdz <56750595+oppabgnamdz@users.noreply.github.com>
2021-07-04 16:06:10 +0700

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -4,7 +4,7 @@ from tobrot.sample_config import Config
 
 class Config(Config):
     TG_BOT_TOKEN= "1800984101:AAGPxhQIKZv_vMWrJcOFOVdJ_gpmHi_FHtM" #imp
-    APP_ID = 2121142 #imp
+    APP_ID = "2121142" #imp
     API_HASH = "73aea46b39f24bd55f967990926c32e9" #imp
     AUTH_CHANNEL = [-1001236695846] #imp replace your_id with your id from telegram or delete
     R_CLONE_CONF_URI = "https://t.me/c/1329348574/566"
