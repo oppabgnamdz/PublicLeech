ccec377b09e95e234759be4983e815a840643217
oppabgnamdz <56750595+oppabgnamdz@users.noreply.github.com>
2021-07-04 15:59:16 +0700

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -3,10 +3,10 @@ from tobrot.sample_config import Config
 #read readme too before filling these stuffs
 
 class Config(Config):
-    TG_BOT_TOKEN= "1545841239:AAEWFgrwdKsLrxVXBTJlgpqzTIAIBceQvDw" #imp
-    APP_ID = 1383845 #imp
-    API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c" #imp
-    AUTH_CHANNEL = [-1001367838165, 1131653685] #imp replace your_id with your id from telegram or delete
+    TG_BOT_TOKEN= "1800984101:AAGPxhQIKZv_vMWrJcOFOVdJ_gpmHi_FHtM" #imp
+    APP_ID = 2121142 #imp
+    API_HASH = "73aea46b39f24bd55f967990926c32e9" #imp
+    AUTH_CHANNEL = [-1001236695846] #imp replace your_id with your id from telegram or delete
     R_CLONE_CONF_URI = "https://t.me/c/1329348574/566"
     R_CLONE_DEST = "/"
     
