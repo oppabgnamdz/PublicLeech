7f049f7cf666474f85518197d909e8f86459b5e6
oppabgnamdz <56750595+oppabgnamdz@users.noreply.github.com>
2021-07-04 16:18:00 +0700

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -4,7 +4,7 @@ from tobrot.sample_config import Config
 
 class Config(Config):
     TG_BOT_TOKEN= "1800984101:AAGPxhQIKZv_vMWrJcOFOVdJ_gpmHi_FHtM" 
-    APP_ID = "2121142" 
+    APP_ID = 2121142 
     API_HASH = "73aea46b39f24bd55f967990926c32e9" 
     AUTH_CHANNEL = [-1001236695846]
     R_CLONE_CONF_URI = "https://t.me/c/1329348574/566"
